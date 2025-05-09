9871db0be203eb1174ece0cd318cb9f772f6b035
Roman Uranov <uranovrg@gmail.com>
2025-05-09 20:55:22 +0300

    init project

---
+++ rest-api-service/src/test/java/com/urantech/restapiservice/RestApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urantech.restapiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
