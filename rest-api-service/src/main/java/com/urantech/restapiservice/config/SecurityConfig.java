c03e80bb1d7ae72a1a4ffecfc6ec82dc1a4263a1
Roman Uranov <uranovrg@gmail.com>
2025-05-11 09:34:34 +0300

    completed all security dependencies

---
+++ rest-api-service/src/main/java/com/urantech/restapiservice/config/SecurityConfig.java
@@ -0,0 +1,70 @@
+package com.urantech.restapiservice.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static com.urantech.restapiservice.model.entity.UserAuthority.Authority.ADMIN;
+import static com.urantech.restapiservice.model.entity.UserAuthority.Authority.USER;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final UserDetailsService userDetailsService;
+
+    private static final String[] AUTH_WHITELIST = {"/api/login", "/api/users/register"};
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(jwtCustomizer -> jwtCustomizer.jwt(
+                        jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(this::convert)))
+                .authorizeHttpRequests(
+                        auth -> auth.requestMatchers(AUTH_WHITELIST)
+                                .permitAll()
+                                .requestMatchers("/api/users")
+                                .hasAnyAuthority(USER.name(), ADMIN.name())
+                                .requestMatchers("/api/tasks")
+                                .authenticated()
+                                .anyRequest()
+                                .hasAnyAuthority(USER.name(), ADMIN.name()));
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(UserDetailsService service) {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(service);
+        provider.setPasswordEncoder(passwordEncoder());
+        return new ProviderManager(provider);
+    }
+
+    private AbstractAuthenticationToken convert(Jwt source) {
+        String email = source.getSubject();
+        UserDetails principal = userDetailsService.loadUserByUsername(email);
+        return new UsernamePasswordAuthenticationToken(principal, null, principal.getAuthorities());
+    }
+}
