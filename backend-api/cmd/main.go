0571e154d783e3bf8f15c7ba8c3c8814fd619111
Roman Uranov <uranovrg@gmail.com>
2026-01-06 17:14:18 +0300

    added graceful shutdown

--- backend-api/cmd/main.go
+++ backend-api/cmd/main.go
@@ -4,8 +4,15 @@ import (
 	"backend-api/internal/config"
 	"backend-api/migrations"
 	"backend-api/pkg/postgres"
+	"context"
+	"errors"
 	"log"
+	"net/http"
+	"os/signal"
+	"syscall"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/pressly/goose/v3"
 )
@@ -32,4 +39,44 @@ func main() {
 		log.Fatal(err)
 	}
 	log.Println("Migrations completed successfully")
+
+	router := chi.NewRouter()
+
+	router.Use(middleware.RequestID)
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
+
+	srv := &http.Server{
+		Addr:    ":" + cfg.Port,
+		Handler: router,
+	}
+
+	startSever(srv)
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	<-ctx.Done()
+
+	shutdownServer(srv)
+}
+
+func startSever(srv *http.Server) {
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("Server failed: %v", err)
+		}
+	}()
+}
+
+func shutdownServer(srv *http.Server) {
+	log.Println("Shutting down server...")
+
+	if err := srv.Shutdown(context.Background()); err != nil {
+		log.Printf("Server shutdown failed: %v", err)
+		panic(err)
+	}
+
+	log.Println("Server shutdown gracefully.")
 }
