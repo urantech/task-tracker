ea4ac1ee07aa1e7d53995324d0ebe5350def3376
Roman Uranov <uranovrg@gmail.com>
2026-01-08 16:29:12 +0300

    refactored

--- backend-api/internal/common/error.go
+++ backend-api/internal/common/error.go
@@ -1,6 +1,9 @@
 package common
 
-import "errors"
+import (
+	"errors"
+	"github.com/go-playground/validator/v10"
+)
 
 var (
 	ErrInvalidRequest     = errors.New("invalid request")
@@ -8,3 +11,38 @@ var (
 	ErrUserNotFound       = errors.New("user not found")
 	ErrInvalidCredentials = errors.New("invalid credentials")
 )
+
+type ValidationError struct {
+	Errors map[string]string
+}
+
+func (e *ValidationError) Error() string {
+	return "validation failed"
+}
+
+func NewValidationError(ve validator.ValidationErrors) *ValidationError {
+	return &ValidationError{
+		Errors: mapValidationErrors(ve),
+	}
+}
+
+func mapValidationErrors(ve validator.ValidationErrors) map[string]string {
+	result := make(map[string]string)
+
+	for _, fe := range ve {
+		switch fe.Tag() {
+		case "required":
+			result[fe.Field()] = "is required"
+		case "email":
+			result[fe.Field()] = "must be a valid email"
+		case "min":
+			result[fe.Field()] = "is too short"
+		case "task_status":
+			result[fe.Field()] = "must be one of: TODO, IN_PROGRESS, DONE"
+		default:
+			result[fe.Field()] = "is invalid"
+		}
+	}
+
+	return result
+}
